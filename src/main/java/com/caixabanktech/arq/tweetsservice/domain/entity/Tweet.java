f0b82a0063f857874279a8725cd7d04c99e58d67
carles <cbescos@silk.es>
2023-05-25 12:22:03 +0200

    fix: mysql

--- src/main/java/com/caixabanktech/arq/tweetsservice/domain/entity/Tweet.java
+++ src/main/java/com/caixabanktech/arq/tweetsservice/domain/entity/Tweet.java
@@ -5,6 +5,7 @@ package com.caixabanktech.arq.tweetsservice.domain.entity;
 import java.util.Date;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
@@ -18,7 +19,8 @@ import lombok.NoArgsConstructor;
 public class Tweet {
 	@Id
 	private UUID id;
-	
+
+	@Column(name="description")
 	private String desc;
 	
 	private String author;
