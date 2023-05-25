8d7f2eb72fcbf8e7298e728a295210bf9efc399d
carles <cbescos@silk.es>
2023-05-25 11:09:19 +0200

    fix: service get/create

--- src/main/java/com/caixabanktech/arq/tweetsservice/domain/repository/TweetRepository.java
+++ src/main/java/com/caixabanktech/arq/tweetsservice/domain/repository/TweetRepository.java
@@ -2,14 +2,18 @@ package com.caixabanktech.arq.tweetsservice.domain.repository;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.caixabanktech.arq.tweetsservice.domain.entity.Tweet;
 
+import java.util.List;
+
 @Repository
 public interface TweetRepository extends JpaRepository<Tweet,UUID>{
-	
-	
+    @Query("select t from Tweet t order by t.createdAt DESC")
+    List<Tweet> findTweets(Pageable pageable);
 
 }
--- src/main/java/com/caixabanktech/arq/tweetsservice/service/TweetService.java
+++ src/main/java/com/caixabanktech/arq/tweetsservice/service/TweetService.java
@@ -1,11 +1,15 @@
 package com.caixabanktech.arq.tweetsservice.service;
 
-import java.util.UUID;
-
-import org.springframework.stereotype.Service;
-
+import com.caixabanktech.arq.tweetsservice.domain.TweetDomain;
+import com.caixabanktech.arq.tweetsservice.domain.entity.Tweet;
 import com.caixabanktech.arq.tweetsservice.domain.mapper.TweetMapper;
 import com.caixabanktech.arq.tweetsservice.domain.repository.TweetRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class TweetService {
@@ -17,7 +21,18 @@ public class TweetService {
 		this.repo=repo;
 		this.mapper=mapper;
 	}
-	
+
+	public List<TweetDomain> getTweets(int page) {
+		Pageable pageable = PageRequest.of(page, 10);
+        List<Tweet> tweets = repo.findTweets(pageable);
+		return mapper.mapListToDomain(tweets);
+	}
+
+	public String createTweet(TweetDomain tweet) {
+		tweet.setId(UUID.randomUUID());
+		Tweet tweetResult = repo.save(mapper.map(tweet));
+		return mapper.map(tweetResult).getId().toString();
+	}
 	
 	/**
 	 * Deletting the tweet from the BBDD
