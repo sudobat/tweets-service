51c4b8ffd69d6432147f564ed948983ae904f6c5
Marc <malvarez@caixabanktech.com>
2023-05-25 10:14:53 +0200

    Primer esqueleto

---
+++ src/main/java/com/caixabanktech/arq/tweetsservice/TweetsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.caixabanktech.arq.tweetsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caixabanktech/arq/tweetsservice/TweetsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caixabanktech.arq.tweetsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
