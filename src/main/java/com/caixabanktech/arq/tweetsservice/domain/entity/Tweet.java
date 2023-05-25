4e8ce629ee8eae7948c012d6200148e1b0a440e1
Marc <malvarez@caixabanktech.com>
2023-05-25 11:05:07 +0200

    Correccion del Tweet

--- src/main/java/com/caixabanktech/arq/tweetsservice/domain/entity/Tweet.java
+++ src/main/java/com/caixabanktech/arq/tweetsservice/domain/entity/Tweet.java
@@ -7,10 +7,14 @@ import java.util.UUID;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Tweet {
 	@Id
 	private UUID id;
--- src/test/java/com/caixabanktech/arq/tweetsservice/service/TweetServiceTest.java
+++ src/test/java/com/caixabanktech/arq/tweetsservice/service/TweetServiceTest.java
@@ -9,7 +9,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
 import com.caixabanktech.arq.tweetsservice.domain.entity.Tweet;
+import com.caixabanktech.arq.tweetsservice.domain.repository.TweetRepository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -18,12 +20,19 @@ public class TweetServiceTest {
     @Autowired
     private TweetService tweetService;
 
-	
+    @Autowired
+    private TweetRepository tweetRepo;	
 	
 	@Test
-	void givenExistentElementDeleteFromDatabase() {
+	void givenExistentElementDeleteFromDatabaseThenValidate() {
 		UUID uuid = UUID.randomUUID();
-	
+		Tweet tweet=new Tweet(uuid,"Tweet","Pepe",new Date());
+		Tweet tweet1=tweetRepo.save(tweet);
+		assertEquals(tweet1,tweet1);
+		assertEquals(tweetRepo.count(),1);
+		tweetService.delete(uuid);
+		
+		assertEquals(tweetRepo.count(),0);
 	}
 	
 	
