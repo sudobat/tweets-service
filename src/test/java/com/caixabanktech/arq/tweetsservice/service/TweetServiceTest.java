1b32c72f6b0345b6b2054076edb2245312b18837
carles <cbescos@silk.es>
2023-05-25 11:09:19 +0200

    fix: service get/create

--- src/test/java/com/caixabanktech/arq/tweetsservice/service/TweetServiceTest.java
+++ src/test/java/com/caixabanktech/arq/tweetsservice/service/TweetServiceTest.java
@@ -3,6 +3,7 @@ package com.caixabanktech.arq.tweetsservice.service;
 import java.util.Date;
 import java.util.UUID;
 
+import com.caixabanktech.arq.tweetsservice.domain.TweetDomain;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +13,7 @@ import com.caixabanktech.arq.tweetsservice.domain.entity.Tweet;
 import com.caixabanktech.arq.tweetsservice.domain.repository.TweetRepository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -35,10 +37,65 @@ public class TweetServiceTest {
 		assertEquals(tweetRepo.count(),0);
 	}
 	
-	
 	@Test
 	void givenNonExistentElementDontDeleteFromDatabase() {
 		UUID uuid = UUID.randomUUID();
 		tweetService.delete(uuid);
 	}
+
+	@Test
+	void givenATweetCreateElementInDataBase() {
+		TweetDomain tweet = new TweetDomain("Tweet", "Pepe", new Date(), null);
+		String uuid = tweetService.createTweet(tweet);
+		assertNotNull(uuid);
+		assertEquals(tweetRepo.count(),1);
+	}
+
+	@Test
+	void givenFirstPageGetTweetsOrderedDesc() {
+		TweetDomain tweet = new TweetDomain("Tweet", "Pepe", new Date(), null);
+		tweetService.createTweet(tweet);
+		tweet = new TweetDomain("Tweet2", "Pepe", new Date(), null);
+		tweetService.createTweet(tweet);
+		tweet = new TweetDomain("Tweet3", "Pepe", new Date(), null);
+		tweetService.createTweet(tweet);
+		tweet = new TweetDomain("Tweet4", "Pepe", new Date(), null);
+		tweetService.createTweet(tweet);
+		tweet = new TweetDomain("Tweet5", "Pepe", new Date(), null);
+		tweetService.createTweet(tweet);
+		assertEquals(tweetRepo.count(),5);
+		assertEquals(tweetService.getTweets(0).get(0).getDesc(), "Tweet5");
+	}
+
+	@Test
+	void givenAnotherPageGetTweetsOrderedDesc() {
+		TweetDomain tweet = new TweetDomain("Tweet", "Pepe", new Date(), null);
+		tweetService.createTweet(tweet);
+		tweet = new TweetDomain("Tweet2", "Pepe", new Date(), null);
+		tweetService.createTweet(tweet);
+		tweet = new TweetDomain("Tweet3", "Pepe", new Date(), null);
+		tweetService.createTweet(tweet);
+		tweet = new TweetDomain("Tweet4", "Pepe", new Date(), null);
+		tweetService.createTweet(tweet);
+		tweet = new TweetDomain("Tweet5", "Pepe", new Date(), null);
+		tweetService.createTweet(tweet);
+		tweet = new TweetDomain("Tweet6", "Pepe", new Date(), null);
+		tweetService.createTweet(tweet);
+		tweet = new TweetDomain("Tweet7", "Pepe", new Date(), null);
+		tweetService.createTweet(tweet);
+		tweet = new TweetDomain("Tweet8", "Pepe", new Date(), null);
+		tweetService.createTweet(tweet);
+		tweet = new TweetDomain("Tweet9", "Pepe", new Date(), null);
+		tweetService.createTweet(tweet);
+		tweet = new TweetDomain("Tweet10", "Pepe", new Date(), null);
+		tweetService.createTweet(tweet);
+		tweet = new TweetDomain("Tweet11", "Pepe", new Date(), null);
+		tweetService.createTweet(tweet);
+		tweet = new TweetDomain("Tweet12", "Pepe", new Date(), null);
+		tweetService.createTweet(tweet);
+		assertEquals(tweetRepo.count(),12);
+		assertEquals(tweetService.getTweets(1).get(0).getDesc(), "Tweet2");
+	}
+
+
 }
