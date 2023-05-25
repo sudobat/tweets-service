92a30fb7ed1d86b70284cfa3ecf915083d2c9d71
carles <cbescos@silk.es>
2023-05-25 10:32:43 +0200

    fix: add service & typo

--- src/main/java/com/caixabanktech/arq/tweetsservice/domain/repository/TweetRepository.java
+++ src/main/java/com/caixabanktech/arq/tweetsservice/domain/repository/TweetRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import com.caixabanktech.arq.tweetsservice.domain.entity.Tweet;
 
 @Repository
-public interface TweetRespository extends JpaRepository<Tweet,String>{
+public interface TweetRepository extends JpaRepository<Tweet,String>{
 	
 	
 
